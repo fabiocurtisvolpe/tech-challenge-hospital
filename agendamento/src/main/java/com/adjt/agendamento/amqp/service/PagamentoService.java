e5a8c81d8a6b1b0ee943dda75d8e991673ed3b92
Fabio Curtis Volpe <curtis.fabio@gmail.com>
2026-02-15 07:10:22 -0300

    ajustes na segurança entre serviços

--- agendamento/src/main/java/com/adjt/agendamento/amqp/service/PagamentoService.java
+++ agendamento/src/main/java/com/adjt/agendamento/amqp/service/PagamentoService.java
@@ -2,27 +2,50 @@ package com.adjt.agendamento.amqp.service;
 
 import com.adjt.agendamento.amqp.config.RabbitConfig;
 import com.adjt.agendamento.amqp.event.ConsultaCriadaEvent;
+import com.adjt.agendamento.amqp.util.MessageSecurityUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.security.interfaces.RSAPrivateKey;
 
 @Slf4j
 @Service
 public class PagamentoService {
 
     private final RabbitTemplate rabbitTemplate;
+    private final ObjectMapper objectMapper;
+    private final RSAPrivateKey privateKey;
 
-    public PagamentoService(RabbitTemplate rabbitTemplate) {
+    public PagamentoService(RabbitTemplate rabbitTemplate,
+                            ObjectMapper objectMapper,
+                            RSAPrivateKey privateKey) {
         this.rabbitTemplate = rabbitTemplate;
+        this.objectMapper = objectMapper;
+        this.privateKey = privateKey;
     }
 
     public void pagamento(Integer idConsulta, Integer idPaciente, BigDecimal valor) {
+        try {
 
-        ConsultaCriadaEvent event = new ConsultaCriadaEvent(idConsulta, idPaciente, valor);
-        log.info("PagamentoService pagamento: {}", event);
+            ConsultaCriadaEvent event = new ConsultaCriadaEvent(idConsulta, idPaciente, valor);
+            log.info("PagamentoService pagamento: {}", event);
 
-        rabbitTemplate.convertAndSend(RabbitConfig.EXCHANGE_PAGAMENTO, RabbitConfig.ROUTING_KEY_CONSULTA_CRIADA, event);
+            String jsonPayload = objectMapper.writeValueAsString(event);
+            String assinatura = MessageSecurityUtil.sign(jsonPayload, this.privateKey);
+
+            rabbitTemplate.convertAndSend(RabbitConfig.EXCHANGE_PAGAMENTO,
+                    RabbitConfig.ROUTING_KEY_CONSULTA_CRIADA, event, message -> {
+                message.getMessageProperties().setHeader("X-Signature", assinatura);
+                message.getMessageProperties().setHeader("X-Service-Source", "ms-agendamento");
+                return message;
+            });
+
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao assinar mensagem para o RabbitMQ", e);
+        }
     }
 }
--- pagamento/src/main/java/com/adjt/pagamento/amqp/consumer/PagamentoConsumer.java
+++ pagamento/src/main/java/com/adjt/pagamento/amqp/consumer/PagamentoConsumer.java
@@ -1,5 +1,6 @@
 package com.adjt.pagamento.amqp.consumer;
 
+import com.adjt.pagamento.amqp.util.MessageSecurityUtil;
 import com.adjt.pagamento.core.enums.StatusPagamentoEnum;
 import com.adjt.pagamento.core.model.Pagamento;
 import com.adjt.pagamento.core.usecase.CadastrarPagamentoUseCase;
@@ -7,10 +8,14 @@ import com.adjt.pagamento.amqp.config.RabbitConfig;
 import com.adjt.pagamento.amqp.dto.event.ConsultaCriadaEvent;
 import com.adjt.pagamento.amqp.dto.event.PagamentoFinalizadoEvent;
 import com.adjt.pagamento.amqp.service.MeioPagamentoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+
+import java.security.interfaces.RSAPublicKey;
 
 @Slf4j
 @Component
@@ -18,21 +23,36 @@ public class PagamentoConsumer {
 
     private final CadastrarPagamentoUseCase cadastrarPagamentoUseCase;
 
+    private final ObjectMapper objectMapper;
     private final MeioPagamentoService meioPagamentoService;
     private final RabbitTemplate rabbitTemplate;
+    private final RSAPublicKey publicKey;
 
     public PagamentoConsumer(CadastrarPagamentoUseCase cadastrarPagamentoUseCase,
+                             ObjectMapper objectMapper,
                              MeioPagamentoService meioPagamentoService,
-                             RabbitTemplate rabbitTemplate) {
+                             RabbitTemplate rabbitTemplate,
+                             RSAPublicKey publicKey) {
 
         this.cadastrarPagamentoUseCase = cadastrarPagamentoUseCase;
+        this.objectMapper = objectMapper;
         this.meioPagamentoService = meioPagamentoService;
         this.rabbitTemplate = rabbitTemplate;
+        this.publicKey = publicKey;
     }
 
     @RabbitListener(queues = RabbitConfig.QUEUE_PAGAMENTO_PROCESSAR)
-    public void receberConsultaParaPagar(ConsultaCriadaEvent event) {
+    public void receberConsultaParaPagar(ConsultaCriadaEvent event, @Header("X-Signature") String signature) {
         try {
+            String jsonPayload = objectMapper.writeValueAsString(event);
+            boolean isValida = MessageSecurityUtil.verify(jsonPayload, signature, this.publicKey);
+
+            if (!isValida) {
+                System.err.println("ALERTA DE SEGURANÇA: Mensagem rejeitada! Assinatura inválida.");
+                return;
+            }
+
+            System.out.println("Mensagem verificada com sucesso! Processando pagamento...");
 
             Pagamento pagamento = Pagamento.builder()
                     .idConsulta(event.consultaId())
@@ -45,16 +65,16 @@ public class PagamentoConsumer {
             log.info("receberConsultaParaPagar: {}", event);
 
             meioPagamentoService.enviarRequisicaoPagamento(event);
-            notificarAgendamento(event.consultaId(), StatusPagamentoEnum.APROVADO_PAGAMENTO);
+            notificarAgendamento(event.consultaId());
 
         } catch (Exception e) {
-            notificarAgendamento(event.consultaId(), StatusPagamentoEnum.REPROVADO_PAGAMENTO);
+            System.err.println("Erro técnico ao validar segurança da mensagem: " + e.getMessage());
         }
     }
 
-    private void notificarAgendamento(Integer id, StatusPagamentoEnum status) {
+    private void notificarAgendamento(Integer id) {
         if (id != null) {
-            var resultado = new PagamentoFinalizadoEvent(id, status);
+            var resultado = new PagamentoFinalizadoEvent(id, StatusPagamentoEnum.APROVADO_PAGAMENTO);
             log.info("notificarAgendamento: {}", resultado);
 
             rabbitTemplate.convertAndSend(RabbitConfig.EXCHANGE_AGENDAMENTO, RabbitConfig.ROUTING_KEY_RESULTADO, resultado);
